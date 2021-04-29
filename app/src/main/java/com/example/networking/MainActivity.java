5add0956cacb62000207ebe288417d18f764c9dc
Novin <a20novra@student.his.se>
2021-04-29 10:36:20 +0200

    Add ArrayAdapter<Mountain>

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.networking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList <Mountain> item;
+    private ArrayAdapter<Mountain> adapter;
 
 
     @Override
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         item = new ArrayList<>();
+        adapter = new ArrayAdapter<Mountain>(this, R.layout.list_item_textview,R.id.my_list_view,item);
 
     }
 }
