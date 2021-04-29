3f1147277ee9236191e1c45cd420e8ee0165f210
Novin <a20novra@student.his.se>
2021-04-29 11:00:02 +0200

    Add private member to Mountain.java / Add getter function

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -1,4 +1,41 @@
 package com.example.networking;
 
-public class Mountain {
+public class Mountain<string> {
+    private String name;
+    private int height;
+    private String description;
+    private double admissionFee;
+
+
+    public Mountain(String name, int height, String description, double admissionFee){
+        this.name = name;
+        this.height = height;
+        this.description = description;
+        this.admissionFee = admissionFee;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+    public double getAdmissionFee(){
+        return admissionFee;
+    }
+
+    @Override
+    public String toString(){
+        return name + " " + admissionFee;
+    }
+
+
+
+
+
 }
